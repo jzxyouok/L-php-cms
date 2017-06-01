3e4b07b52730fd8bd8dca093f2dd748dae122213
lovelife10000 <1358180015@qq.com>
2017-06-01 21:28:35 +0800

    创建laravel项目

---
+++ app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello',function(){
+  return "Hello Laravel[GET]!";
+});
+
+Route::get('/testPost',function(){
+  $csrf_token = csrf_token();
+  $form = <<<FORM
+        <form action="/L-php-cms/public/index.php/hello" method="POST">
+            <input type="hidden" name="_token" value="{$csrf_token}">
+            <input type="submit" value="Test"/>
+        </form>
+FORM;
+  return $form;
+});
+
