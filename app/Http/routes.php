2d1b5d78ca0050ee96a475bd5a86929e47f2b920
lovelife10000 <1358180015@qq.com>
2017-06-07 18:49:18 +0800

    添加模版

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,48 +1,82 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
+Route::get('/',function(){
   return view('welcome');
 });
-Route::get('/admin/manage', function () {
-  return view('admin.basic_info');
-});
-
-
-
 
 /*
- * 用户组
+ * 仪表盘
  * */
 Route::group(['namespace' => 'Admin'], function () {
 
   Route::get('/admin/manage/admin_user_group_add','adminUserGroupAddController@view')->name('admin_user_group_add');
   Route::post('/admin/manage/admin_user_group_add/add','adminUserGroupAddController@add')->name('admin_user_group_add_add');
-
   Route::get('/admin/manage/admin_user_group_all','adminUserAllController@showProfile')->name('admin_user_group_all');
 
 });
 
 
+/*
+ * 用户管理
+ * */
+Route::group(['namespace' => 'Admin'], function () {
+
+  Route::get('/admin/manage/users_manage/admin_user_group_add','adminUserGroupAddController@view')->name('admin_user_group_add');
+  Route::post('/admin/manage/users_manage/admin_user_group_all','adminUserGroupAddController@add')->name('admin_user_group_all');
+  Route::get('/admin/manage/users_manage/admin_user_add','adminUserGroupAddController@add')->name('admin_user_add');
+  Route::get('/admin/manage/users_manage/admin_user_all','adminUserGroupAddController@add')->name('admin_user_all');
+});
+
+
 
 /*
- * 用户
+ * 文档管理
  * */
 Route::group(['middleware' => 'auth'], function () {
 
   Route::get('/admin/manage/admin_user_all','adminUserAllController@showProfile')->name('admin_user_all');
+  Route::get('/admin/manage/admin_user_add','adminUserAllController@showProfile')->name('admin_user_add');
+
+});
+
+/*
+ * 文件管理
+ * */
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/admin/manage/admin_user_all','adminUserAllController@showProfile')->name('admin_user_all');
+  Route::get('/admin/manage/admin_user_add','adminUserAllController@showProfile')->name('admin_user_add');
+
+});
+
+/*
+ * 数据管理
+ * */
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/admin/manage/admin_user_all','adminUserAllController@showProfile')->name('admin_user_all');
+  Route::get('/admin/manage/admin_user_add','adminUserAllController@showProfile')->name('admin_user_add');
+
+});
 
 
+/*
+ * 定制中心
+ * */
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/admin/manage/admin_user_all','adminUserAllController@showProfile')->name('admin_user_all');
+  Route::get('/admin/manage/admin_user_add','adminUserAllController@showProfile')->name('admin_user_add');
+
+});
+
+
+/*
+ * 系统设置
+ * */
+Route::group(['middleware' => 'auth'], function () {
+
+  Route::get('/admin/manage/admin_user_all','adminUserAllController@showProfile')->name('admin_user_all');
   Route::get('/admin/manage/admin_user_add','adminUserAllController@showProfile')->name('admin_user_add');
 
 });
