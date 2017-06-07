1ddf185b2674f08810a8bc4025a7718a79c1c5d5
lovelife10000 <1358180015@qq.com>
2017-06-07 16:05:36 +0800

    数据库操作入门

--- app/Http/Controllers/Admin/adminUserGroupAddController.php
+++ app/Http/Controllers/Admin/adminUserGroupAddController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Util\Settings;
+use Illuminate\Support\Facades\DB;
 
 class adminUserGroupAddController extends Controller
 {
@@ -18,14 +19,15 @@ class adminUserGroupAddController extends Controller
   }
 
   public function add(Request $request){
-    $name = $request->input('name');
-    $pid = $request->input('pid');
-    echo $pid;
+//    $name = $request->input('name');
+//    $pid = $request->input('pid');
+//    echo $pid;
 
-//    DB::table('admin_user_groups')->insert([
-//      ['id'=>1,'name'=>'超级管理员','remark'=>'laravel@test.com','group_id'=>'0','pid'=>'0','status'=>'1'],
-//
-//    ]);
+//    DB::connection();
+    DB::table('admin_user_groups')->insert([
+      ['id'=>1,'name'=>'超级管理员','remark'=>'laravel@test.com','group_id'=>'0','pid'=>'0','status'=>'1'],
+
+    ]);
   }
 
 
