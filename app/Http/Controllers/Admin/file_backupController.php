facb21650ec9eb258b71ad72bcf3c4eb653e15cd
lovelife10000 <1358180015@qq.com>
2017-06-08 10:51:42 +0800

    配置所有控制器

---
+++ app/Http/Controllers/Admin/file_backupController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class file_backupController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Admin/mediaManageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class mediaManageController extends Controller
+{
+    //
+}
