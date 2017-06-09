e85b1abd2c2301c6c669dc297e668144b29a6262
lovelife10000 <1358180015@qq.com>
2017-06-09 10:09:01 +0800

    验证码功能

--- app/Http/Controllers/Admin/adminLoginController.php
+++ app/Http/Controllers/Admin/adminLoginController.php
@@ -1,32 +1,41 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use App\Http\Model\AdminUser;
-
+use Gregwar\Captcha\CaptchaBuilder;
 
 class adminLoginController extends Controller
 {
-    public function view(){
-      return view('admin.admin_login', [
+  public function view()
+  {
+    /*
+     * 创建验证码
+     * */
+    $builder = new CaptchaBuilder;
+    $builder->build();
+    return view('admin.admin_login', [
+      'builder'=>$builder
+    ]);
+  }
 
-      ]);
-    }
+  public function gotoLogin()
+  {
+    return redirect()->route('admin_login');
+  }
 
-    public function gotoLogin(){
-      return redirect()->route('admin_login');
-    }
-
-    public function login(Request $request){
-      $name = $request->input('name');
-      //DB::table('admin_users')
+  public function login(Request $request)
+  {
+    $name = $request->input('name');
+    //DB::table('admin_users')
 //        $user=AdminUser::first();
 //        if($user->username===){
 //
 //        }
-    }
+  }
 
 }
--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -24,7 +24,7 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">填写用户名及密码</p>
+    <p class="login-box-msg">用户登录</p>
 
     <form action="admin_login" method="post" onsubmit="return false;">
       <div class="form-group has-feedback">
@@ -34,6 +34,16 @@
       <div class="form-group has-feedback">
         <input type="password" class="form-control" name="password" placeholder="请输入密码" ng-model="password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="code" placeholder="请输入验证码" ng-model="code">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <img src="<?php echo $builder->inline(); ?>" alt="">
+        </div>
+
       </div>
       <div class="row">
         <div class="col-xs-8">
